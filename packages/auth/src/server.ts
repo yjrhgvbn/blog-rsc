9fbb745452bce8d87ba16b664c2e2a81b92d7cbe
wu <yjrhgvbn@gmail.com>
2023-12-08 23:00:38 +0800

    init: add db

---
+++ packages/auth/src/server.ts
@@ -0,0 +1,111 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import type { Role, RoleTypes } from "@repo/db/types";
+import { type DefaultSession } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { prisma } from "@repo/db";
+import NextAuth from "./next-auth";
+
+export type { Session, DefaultSession as DefaultAuthSession } from "next-auth";
+
+/**
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user?: DefaultSession["user"] & {
+      id: string;
+      role: RoleTypes[];
+    };
+  }
+
+  interface User {
+    createdAt: Date;
+    roles: Role[];
+  }
+}
+
+if (!process.env.GITHUB_ID) {
+  throw new Error("No GITHUB_ID has been provided.");
+}
+
+if (!process.env.GITHUB_SECRET) {
+  throw new Error("No GITHUB_SECRET has been provided.");
+}
+
+const useSecureCookies = process.env.VERCEL_ENV === "production";
+const cookiePrefix = useSecureCookies ? "__Secure-" : "";
+const cookieDomain = useSecureCookies ? "typehero.dev" : undefined;
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  pages: {
+    signIn: "/login",
+  },
+  cookies: {
+    sessionToken: {
+      name: `${cookiePrefix}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        domain: cookieDomain,
+        secure: useSecureCookies,
+      },
+    },
+  },
+  callbacks: {
+    session: async ({ session, user }) => {
+      let userRoles: RoleTypes[] = [];
+      if (user.roles) {
+        userRoles = user.roles.reduce((acc: RoleTypes[], role) => {
+          acc.push(role.role);
+          return acc;
+        }, []);
+      }
+      if (!userRoles.includes("USER")) {
+        const updatedUser = await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            roles: {
+              connectOrCreate: {
+                where: {
+                  role: "USER",
+                },
+                create: {
+                  role: "USER",
+                },
+              },
+            },
+          },
+          include: {
+            roles: true,
+          },
+        });
+        userRoles = updatedUser.roles.reduce((acc: RoleTypes[], role) => {
+          acc.push(role.role);
+          return acc;
+        }, []);
+      }
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          role: userRoles,
+          createAt: user.createdAt,
+        },
+      };
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+});
